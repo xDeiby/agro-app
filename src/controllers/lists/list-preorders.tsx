30907f76392276519c3540d3760f89b592a8456a
Deiby C <deiby.cabrera@alumnos.uv.cl>
2021-02-28 12:52:31 -0300

    errores en build sin sentido

--- src/controllers/lists/list-preorders.tsx
+++ src/controllers/lists/list-preorders.tsx
@@ -1,7 +1,7 @@
 import { EntityRelated } from "@trifenix/agro-data";
+import List from "../../components/list/List";
 import { withList } from "../../HightOrderComponent/lists/withList";
 import { searchInstance } from "../../services/azure-search/indexs-instances/AgroSearch";
-import List from "../../components/list/list";
 const ListPreorders = withList({
 	getItems: async (entity: EntityRelated, entity_rel: EntityRelated, id: string) => {
 		const entities = (await searchInstance.getRelIn(entity, entity_rel, id)).data;
